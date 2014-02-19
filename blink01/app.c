bf6bea6cc86ba47bb58e500e2a2aad37c60b5271
Anatol Pomozov <anatol.pomozov@gmail.com>
2014-02-19 15:19:43 -0800

    Initialize BSS/data sections in RAM

--- blink01/app.c
+++ blink01/app.c
@@ -1,14 +1,46 @@
 #include <stdint.h>
 #include <stdbool.h>
 
-extern uint32_t _stack_start;
-void application(void);
+extern uint32_t __stack_begin__;
+extern uint32_t __romdata_begin__;
+extern uint32_t __data_begin__;
+extern uint32_t __data_end__;
+extern uint32_t __bss_begin__;
+extern uint32_t __bss_end__;
 
-__attribute__ ((section(".vector_table")))
+void application(void);
+void crt0(void);
+
+__attribute__ ((section(".startup")))
 const void *vector_table[] = {
-    &_stack_start, // Stack Pointer
-    &application   // Reset Vector
+    &__stack_begin__, // Stack Pointer
+    &crt0   // Reset Vector
 };
+
+void crt0(void) {
+  // initialize data section
+  uint32_t *src = &__romdata_begin__;
+  uint32_t *dest = &__data_begin__;
+
+  while(dest < &__data_end__) {
+    *dest = *src;
+
+    src++;
+    dest++;
+  }
+
+  // initialize bss with zero
+  dest = &__bss_begin__;
+  while (dest < &__bss_end__) {
+    *dest = 0;
+    dest++;
+  }
+
+  application();
+
+  while(true) {};
+}
+
 
 #define RCCBASE   0x40023800
 #define GPIODBASE 0x40020C00
@@ -40,7 +72,7 @@ void application() {
 
   while(true) {
     MEM(GPIODBASE + 0x18) = 0xE0001000;
-    wait(1);
+    wait(10);
 //  MEM(GPIODBASE + 0x18) = 0xD0002000;
 //  wait(4);
     MEM(GPIODBASE + 0x18) = 0xB0004000;
