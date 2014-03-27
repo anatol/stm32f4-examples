486277968e5c69a204a9c8d6663924e77e8529f7
Anatol Pomozov <anatol.pomozov@gmail.com>
2014-03-27 15:09:58 -0700

    Add references to STM32 documentation

--- blink01/app.c
+++ blink01/app.c
@@ -1,7 +1,11 @@
 #include <stdint.h>
 #include <stdbool.h>
 
+// base address for
+// see reference manual section 2.3
 #define RCCBASE   0x40023800
+// base address for GPIO D
+// see reference manual section 2.3
 #define GPIODBASE 0x40020C00
 
 #define MEM(address) (*(volatile uint32_t *)(address))
@@ -15,21 +19,26 @@ void wait(int period) {
 void application() {
   uint32_t ra;
 
-  ra = MEM(RCCBASE + 0x30);
-  ra |= 1 << 3; //enable port D
-  MEM(RCCBASE + 0x30) = ra;
+  // enable GPIO port D
+  // see refernce manual section 7.3.10
+  MEM(RCCBASE + 0x30) |= (1 << 3);
 
-  //d12 = d15 output
+  // configure pins D12-D15 to be general purpose
+  // see reference manual 8.4.1
   ra = MEM(GPIODBASE + 0x00);
   ra &= 0x00FFFFFF;
   ra |= 0x55000000;
   MEM(GPIODBASE + 0x00) = ra;
-  //push pull
+
+  // configure pins D12-D15 to be push-pull
+  // see reference manual 8.4.2
   ra = MEM(GPIODBASE + 0x04);
   ra &= 0xFFFF0FFF;
   MEM(GPIODBASE + 0x04) = ra;
 
   while(true) {
+    // disable D13-15 and enable D15
+    // see reference manual 8.4.7
     MEM(GPIODBASE + 0x18) = 0xE0001000;
     wait(10);
 //  MEM(GPIODBASE + 0x18) = 0xD0002000;
